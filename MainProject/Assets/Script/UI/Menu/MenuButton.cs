1b3c358d96a92e8e036e8ca55c6a8e487b164b3a
Guiny-Time <1084926982@qq.com>
2021-05-20 16:24:59 +0800

    字体替换与UI完善

--- MainProject/Assets/Script/UI/Menu/MenuButton.cs
+++ MainProject/Assets/Script/UI/Menu/MenuButton.cs
@@ -6,13 +6,17 @@ public class MenuButton : MonoBehaviour
 {
     private bool isOpen = false;
     [SerializeReference] private GameObject menuPanel;
+    [SerializeReference] private GameObject exitConfirm;
+    [SerializeReference] private GameObject volumeControl;
+    Stack<GameObject> PanelList = new Stack<GameObject>();
 
+
+    // 防止鼠标外移
     private void Awake()
     {
         Cursor.lockState = CursorLockMode.Confined;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape)) ShowPanel();
@@ -20,17 +24,50 @@ public class MenuButton : MonoBehaviour
 
     public void ShowPanel()
     {
+        // 底层面板打开时，按esc关闭
         if (isOpen)
         {
-            isOpen = false;
+            PanelList.Peek().SetActive(false);
+            PanelList.Pop();
+            if(PanelList.Count == 0)
+                isOpen = false;
             Cursor.lockState = CursorLockMode.Confined;
         }
-        else
+        // 底层面板关闭时，按esc打开
+        if(!isOpen)
         {
+            PanelList.Push(menuPanel);
+            PanelList.Peek().SetActive(true);
             isOpen = true;
             Cursor.lockState = CursorLockMode.None;
         }
-
         menuPanel.SetActive(isOpen);
+    }
+
+    // 展示退出面板面板
+    public void ShowExitConfirmPanel()
+    {
+        PanelList.Push(exitConfirm);
+        PanelList.Peek().SetActive(true);
+    }
+
+    // 展示音量调节面板
+    public void ShowVolumePanel()
+    {
+        PanelList.Push(volumeControl);
+        PanelList.Peek().SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    
+    public void BackGame()
+    {
+        PanelList.Peek().SetActive(false);
+        PanelList.Pop();
+        isOpen = false;
+        Cursor.lockState = CursorLockMode.Confined;        
     }
 }
